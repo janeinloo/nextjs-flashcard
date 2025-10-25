7b1a6fbdf7cf6f796df95143ab8800c4e9f25d0b
Jan Aaron Einloo <janeinlo@tlu.ee>
2025-10-25 23:16:18 +0300

    feat(stats): add Stats page to display flashcards attempts summary

---
+++ app/flashcard/stats/page.tsx
@@ -0,0 +1,10 @@
+import Stats from "./Stats";
+
+export default function StatsPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Flashcards Statistics</h1>
+      <Stats />
+    </div>
+  );
+}
